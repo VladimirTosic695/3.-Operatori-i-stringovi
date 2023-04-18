b324ebf3d6169667d09d11b6bdc861d039a7ac8b
VladimirTosic695 <131188611+VladimirTosic695@users.noreply.github.com>
2023-04-18 19:41:18 +0200

    Add files via upload

---
+++ Operatori/src/operatori/Main.java
@@ -0,0 +1,34 @@
+package operatori;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+int a = 5;
+int b = 50;
+int zbir  = a + b;
+System.out.println("Vrednost promenljive zbir je " + zbir);
+// a i b su operandi a +,-,=,*,/ su operatori
+int razlika = a - b;
+System.out.println("Razlika je = " + razlika);
+
+int proizvod = a*b;
+System.out.println("Proizvod dve vrednosti je " + proizvod);
+
+int kolicnik = b / a;
+System.out.println("Kolicnik je = " + kolicnik);
+
+float kolicnika = b/a;
+System.out.println(kolicnika);
+int ostatak = 10 % 3;
+System.out.println("Ostatak je " + ostatak);
+
+float tacnoDeljenje = 17f / 3f;
+System.out.println("tacno deljenje je " + tacnoDeljenje);
+
+double provera = (double)50/3;
+System.out.println(provera);
+
+	}
+
+}
